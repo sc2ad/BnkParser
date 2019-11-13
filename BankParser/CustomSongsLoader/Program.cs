e87822d6b774887521ef962c488fc4927aac4c30
Adam ? <adamznow@gmail.com>
2019-11-13 00:21:55 -0800

    .gitignore updates, actually add Program.cs to CustomSongsLoader

---
+++ BankParser/CustomSongsLoader/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace CustomSongsLoader
+{
+    class Invocation
+    {
+
+    }
+    class InvocationResult
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString;
+            using (StreamReader reader = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding))
+            {
+                jsonString = reader.ReadToEnd();
+            }
+            Invocation inv = JsonConvert.DeserializeObject<Invocation>(jsonString);
+            InvocationResult res = Program.RunInvocation(inv);
+            string jsonOut = JsonConvert.SerializeObject(res, Formatting.None);
+            Console.WriteLine(jsonOut);
+        }
+    }
+}
